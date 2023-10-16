cbca1ca29641a81e7f4dcd52edbc86179f5e0f3f
Sejin <118341060+szzzing@users.noreply.github.com>
2023-10-16 14:27:05 +0900

    [level 3] Title: 징검다리 건너기, Time: 16.52 ms, Memory: 61.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/64062. 징검다리 건너기/징검다리 건너기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public static int solution(int[] stones, int k) {
+		int answer=0;
+		int min=1;
+		int max=200000000;
+		int mid=0;
+
+		while(min<=max) {
+			mid=(max+min)/2;
+			if(cross(mid, stones, k)) {
+				min=mid+1;
+				answer=mid;
+			} else {
+				max=mid-1;
+			}
+		}
+		return answer;
+	}
+	
+	public static boolean cross(int friends, int[] stones, int k) {
+		int count=0;
+
+		for (int stone:stones) {
+        
+			if (friends>stone) count++;
+			else count=0;
+
+			if (count==k) return false;
+		}
+		return true;
+	}
+}
